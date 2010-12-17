52c7dd614d08fe77fbef2ece7338550c1811f485
Xenofon Foukas <foukas.xenofon@gmail.com>
2010-12-17 12:32:07 +0200

    Added main menu and created the activities needed

--- gen/edu/aueb/cs/uw/R.java
+++ gen/edu/aueb/cs/uw/R.java
@@ -14,13 +14,25 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int about=0x7f050004;
         public static final int board_view=0x7f050000;
+        public static final int exit=0x7f050005;
+        public static final int new_game=0x7f050002;
+        public static final int quick_start=0x7f050003;
+        public static final int widget0=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int board=0x7f030001;
+        public static final int configs=0x7f030002;
+        public static final int menu=0x7f030003;
     }
     public static final class string {
+        public static final int about=0x7f040003;
         public static final int app_name=0x7f040001;
+        public static final int exit=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int new_game=0x7f040002;
+        public static final int quick_start=0x7f040005;
     }
 }
--- src/edu/aueb/cs/uw/Upwords.java
+++ src/edu/aueb/cs/uw/Upwords.java
@@ -1,13 +1,52 @@
 package edu.aueb.cs.uw;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Upwords extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.menu);
+        
+        Button NewGameButton = (Button)findViewById(R.id.new_game);
+        NewGameButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent StartGameIntent = new Intent(Upwords.this,Configs.class);
+        		startActivity(StartGameIntent);
+        	}
+        });
+        
+        Button QuickStartButton = (Button)findViewById(R.id.quick_start);
+        QuickStartButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent StartGameIntent = new Intent(Upwords.this,Board.class);
+        		startActivity(StartGameIntent);
+        	}
+        });
+        
+        Button AboutButton = (Button)findViewById(R.id.about);
+        AboutButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent StartGameIntent = new Intent(Upwords.this,About.class);
+        		startActivity(StartGameIntent);
+        	}
+        });
+        
+        Button ExitButton = (Button)findViewById(R.id.exit);
+        ExitButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		System.exit(0);
+        	}
+        });
     }
 }

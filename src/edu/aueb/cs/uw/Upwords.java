d136953a3ed0589938dd5bcd04b747c99c859a17
Xenofon Foukas <foukas.xenofon@gmail.com>
2010-12-21 15:39:50 +0200

    Added dialog window for exit action

--- src/edu/aueb/cs/uw/Upwords.java
+++ src/edu/aueb/cs/uw/Upwords.java
@@ -1,18 +1,22 @@
 package edu.aueb.cs.uw;
 
 
-import edu.aueb.cs.uw.core.GameConfigs;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import edu.aueb.cs.uw.core.GameConfigs;
 
 public class Upwords extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
         
@@ -55,7 +59,21 @@ public class Upwords extends Activity {
         ExitButton.setOnClickListener(new OnClickListener() {
         	
         	public void onClick(View v) {
-        		System.exit(0);
+        		AlertDialog.Builder builder = new AlertDialog.Builder(Upwords.this);
+        		builder.setMessage("Are you sure you want to exit?")
+        		       .setCancelable(false)
+        		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        		           public void onClick(DialogInterface dialog, int id) {
+        		                Upwords.this.finish();
+        		           }
+        		       })
+        		       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+        		           public void onClick(DialogInterface dialog, int id) {
+        		                dialog.cancel();
+        		           }
+        		       });
+        		AlertDialog alert = builder.create();
+        		alert.show();
         	}
         	
         });

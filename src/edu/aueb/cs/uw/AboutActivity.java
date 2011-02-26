de48c586b5a57da4d4ceb8570e5164f5bb801efe
Xenofon Foukas <fon1989@gmail.com>
2011-02-26 23:55:38 +0200

    Fixed layout in about and in instructions

--- src/edu/aueb/cs/uw/AboutActivity.java
+++ src/edu/aueb/cs/uw/AboutActivity.java
@@ -2,14 +2,27 @@ package edu.aueb.cs.uw;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AboutActivity extends Activity{
 
+	private Button backButton;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        this.backButton=(Button)this.findViewById(R.id.back_about);
+        this.backButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
     }
 	
 }
--- src/edu/aueb/cs/uw/InstructionsActivity.java
+++ src/edu/aueb/cs/uw/InstructionsActivity.java
@@ -2,13 +2,26 @@ package edu.aueb.cs.uw;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class InstructionsActivity extends Activity
 {
+	private Button backButton;
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.instructions);
-	}
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructions);
+        this.backButton=(Button)this.findViewById(R.id.back_instructions);
+        this.backButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+    }
 }
